4f876f24f85fa16e50f191163f8102482f1efde5
Andrea Schinoppi <andrea.schinoppi@studio.unibo.it>
2022-02-14 15:21:21 +0100

    Gestione immagini nuovi utenti client side

--- html/public/Back/addElem.js
+++ html/public/Back/addElem.js
@@ -8,11 +8,17 @@ async function renderAddelem() {
       `)
 }
 
-async function renderaddPerson(type) {
+async function renderaddPerson() {
+  let url = serverUrl + 'mongo/posthere'
+  let type = 'user';
     $("#textdiv").empty();
     $("#textdiv").append(`
     <h3>Aggiungi un utente</h3>
 
+    <form method='post' action="${url}?type=${type}" enctype="multipart/form-data" id="tosub">
+
+    <fieldset>
+     <legend>Tipo di utente da inserire</legend>
     <div class="form-check">
         <input class="form-check-input" type="radio" value="user" name="Radiogrouptype" id="user" checked>
         <label class="form-check-label" for="user">
@@ -33,112 +39,128 @@ async function renderaddPerson(type) {
           Manager
         </label>
     </div>
+    </fieldset>
 
-    <form>
+
+    <div class="mb-3">
+      <label for="image" class="form-label">Immagine profilo</label>
+        <input type='file' class="imgup" id="image" name="image" aria-describedby="imghelp"></input>
+        <div id="imghelp" class="form-text">Immagine profilo dell'utente</div>
+      </div>
+
         <div class="mb-3">
           <label for="name" class="form-label">Nome</label>
-          <input type="text" class="form-control" placeholder="Nome dell'utente" id="name" aria-describedby="namehelp">
+          <input type="text" class="form-control" placeholder="Nome dell'utente" id="name" name="nome" aria-describedby="namehelp">
           <div id="namehelp" class="form-text">Nome dell'utente</div>
         </div>
 
       <div class="mb-3">
       <label for="address" class="form-label">Indirizzo</label>
-      <input type="text" class="form-control" placeholder="Indirizzo dell'utente" id="address" aria-describedby="addresshelp">
+      <input type="text" class="form-control" placeholder="Indirizzo dell'utente" id="address" name="address" aria-describedby="addresshelp">
       <div id="addresshelp" class="form-text">Indirizzo dell'utente</div>
     </div>
 
       <div class="mb-3">
       <label for="address" class="form-label">Mail</label>
-      <input type="email" class="form-control" placeholder="Mail dell'utente" id="mail" aria-describedby="mailhelp">
+      <input type="email" class="form-control" placeholder="Mail dell'utente" id="mail" name="mail" aria-describedby="mailhelp">
       <div id="mailhelp" class="form-text">Mail dell'utente</div>
     </div>
 
-    <button type="submit" class="btn btn-primary" onclick="addPerson()">Aggiungi</button>
+    <button type="submit" class="btn btn-primary" >Aggiungi</button>
     <button type="button" class="btn btn-warning" onclick="renderAddelem()">Annulla</button>
 </form>
 `);
 
     $("form input").attr("required", true);
+    $("#image").prop("required",false);
+
+
+    $("input[type='radio']").change(function(){
+    if ($("input[type='radio']:checked").val()!="user")
+    {
+      $("#image").prop("disabled",true);
+      $("#image").prop("aria-disabled",true);
+    }
+    else
+    {
+      $("#image").prop("disabled",false);
+      $("#image").prop("aria-disabled",false);
+    }
+    type = $("input[type='radio']:checked").val();
+  });
+
 
     $("form").submit(function (e) {
         e.preventDefault();
-    });
 
+        let form = $('#tosub')[0];
+
+        // Create an FormData object
+        let data = new FormData(form);
+
+        $.ajax({
+            type: "POST",
+            enctype: 'multipart/form-data',
+            url: `${url}?type=${type}`,
+            data: data,
+            processData: false,
+            contentType: false,
+            success: function (data) {
+                renderAddelem();
+                $("#textdiv").prepend(`<div class="success_upd">${data.msg}</div>`);
+                setTimeout(function () { $(".success_upd").remove() }, 10000);
+            },
+            error: function (data) {
+                $("#textdiv").prepend(`<div class="fail_upd">Inserimento fallito</div>`);
+                setTimeout(function () { $(".fail_upd").remove() }, 10000);
+            }
+        });
+      });
 }
-async function addPerson() {
 
-    let chkd = $("input[type='radio']:checked").val();
 
-    let persondata = {
-        "nome": $(`#name`).val(),
-        "indirizzo": $(`#address`).val(),
-        "mail": $(`#mail`).val(),
-        "psw": "default",
-        "tier": "0",
-        "img": "../img/avatar.jpg",
-        "annotazioni": "",
-        "storico": []
-    };
-
-    for (input of $("input")) {
-        if (!(input.checkValidity())) {
-            $(input).css({ "border": "1px solid red" });
-            return;
-        }
-    }
-
-    await $.ajax({
-        url: serverUrl + `mongo/posthere?type=${chkd}`,
-        type: 'POST',
-        data: JSON.stringify(persondata),
-        crossDomain: true,
-        contentType: 'application/json',
-        success: async function (data) {
-            await renderAddelem();
-            $("#textdiv").prepend(`<div class="success_upd">${data.msg}</div>`);
-            setTimeout(function () { $(".success_upd").remove() }, 10000);
-        },
-        error: function (data) {
-            $("#textdiv").prepend(`<div class="fail_upd">${data}</div>`);
-            setTimeout(function () { $(".fail_upd").remove() }, 10000);
-        }
-    });
-
-}
 
 async function renderaddOffice() {
+  let url = serverUrl + 'mongo/posthere'
     $("#textdiv").empty();
     $("#textdiv").append(`
     <h3>Aggiungi un ufficio</h3>
 
-    <form>
+    <form method='post' action="${url}?type=uffici" enctype="multipart/form-data" id="tosub_off">
+
+    <div class="mb-3">
+      <label for="image" class="form-label">Immagine dell'ufficio</label>
+        <input type='file' class="imgup" id="image" name="image" aria-describedby="imghelp" required></input>
+        <div id="imghelp" class="form-text">Immagine dell'ufficio</div>
+      </div>
+
         <div class="mb-3">
           <label for="name" class="form-label">Nome</label>
-          <input type="text" class="form-control" placeholder="Nome dell'ufficio" id="name" aria-describedby="namehelp">
+          <input type="text" class="form-control" placeholder="Nome dell'ufficio" id="name" name="nome" aria-describedby="namehelp">
           <div id="namehelp" class="form-text">Nome dell'ufficio</div>
         </div>
 
       <div class="mb-3">
       <label for="address" class="form-label">Indirizzo</label>
-      <input type="text" class="form-control" placeholder="Indirizzo dell'ufficio" id="address" aria-describedby="addresshelp">
+      <input type="text" class="form-control" placeholder="Indirizzo dell'ufficio" id="address" name="indirizzo" aria-describedby="addresshelp">
       <div id="addresshelp" class="form-text">Indirizzo dell'ufficio</div>
     </div>
 
     <div class="mb-3">
       <label for="mq" class="form-label">Metri quadri</label>
-      <input type="number" min="20" max="500" class="form-control" placeholder="Grandezza dell'ufficio in MQ" id="mq" value="40" aria-describedby="mqhelp">
+      <input type="number" min="20" max="500" class="form-control" placeholder="Grandezza dell'ufficio in MQ" id="mq" name="mq" value="80" aria-describedby="mqhelp">
       <div id="mqhelp" class="form-text">Metri quadri dell'ufficio</div>
     </div>
 
     <div class="mb-3">
       <label for="tier" class="form-label">Tier</label>
-      <input type="number" min="1" max="3" class="form-control" placeholder="Tier dell'ufficio" id="tier" value="2" aria-describedby="tierhelp">
+      <input type="number" min="1" max="3" class="form-control" placeholder="Tier dell'ufficio" id="tier" name="tier" value="2" aria-describedby="tierhelp">
       <div id="tierhelp" class="form-text">Tier dell'ufficio</div>
     </div>
 
     <div class="mb-3">
     <label for="stato" class="form-label">Stato</label>
-    <select class="form-select selform" aria-label="Selezione stato dell'ufficio" id="stato" aria-describedby="statushelp">
+    <select class="form-select selform" aria-label="Selezione stato dell'ufficio" id="stato" name="stato" aria-describedby="statushelp">
       <option value="pessimo">Pessimo</option>
       <option value="sufficiente">Sufficiente</option>
       <option selected value="buono">Buono</option>
@@ -149,25 +171,21 @@ async function renderaddOffice() {
 
     <div class="mb-3">
       <label for="costo" class="form-label">Costo base</label>
-      <input type="number" min="20" max="500" class="form-control" placeholder="Costo base dell'ufficio" id="costo" value="120" aria-describedby="costohelp">
+      <input type="number" min="20" max="500" class="form-control" placeholder="Costo base dell'ufficio" id="costo" name="costo_base" value="120" aria-describedby="costohelp">
       <div id="costohelp" class="form-text">Costo base dell'ufficio</div>
     </div>
 
     <div class="mb-3">
     <label for="desc" class="form-label">Descrizione</label>
-    <input type="text" class="form-control" placeholder="Descrizione dell'ufficio" id="desc" aria-describedby="descriptionhelp">
+    <textarea class="form-control" aria-label="Descrizione ufficio" id="desc" name="descrizione" aria-describedby="descriptionhelp" required></textarea>
     <div id="descriptionhelp" class="form-text">Descrizione dell'ufficio</div>
     </div>
 
     <div class="mb-3">
       <label for="ann" class="form-label">Annotazioni</label>
-      <input type="text" class="form-control" placeholder="Annotazioni riguardo l'ufficio" id="ann" aria-describedby="annhelp">
+      <textarea class="form-control" aria-label="Annotazioni" id="ann" name="annotazione" aria-describedby="annhelp"></textarea>
       <div id="annhelp" class="form-text">Annotazioni riguardo l'ufficio</div>
     </div>
-
-<div class="disponibilita">
-    <button type="button" class="btn btn-success disp-btn" onclick="disp(${-1})">Modifica disponibilità </button>
-</div>
 
         <button type="submit" class="btn btn-primary" onclick="addoffice()">Aggiungi</button>
         <button type="button" class="btn btn-warning" onclick="renderAddelem()">Annulla</button>
@@ -178,51 +196,28 @@ async function renderaddOffice() {
 
     $("form").submit(function (e) {
         e.preventDefault();
-    });
-}
 
-async function addoffice() {
-    let occupato = [];
+        let form = $('#tosub_off')[0];
 
-    if ($("#start0").val() != undefined && $("#end0").val() != undefined) 
-    {
-        occupato.push({ "from": $("#start0").val(), "to": $("#end0").val() })
-    }
-    let formdata = {
-        "nome": $(`#name`).val(),
-        "indirizzo": $(`#address`).val(),
-        "occupato": occupato,
-        "mq": $(`#mq`).val(),
-        "tier": $(`#tier`).val(),
-        "stato": $(`#stato`).val(),
-        "costo_base": $(`#costo`).val(),
-        "img": "../img/Ufficio.jpg",
-        "descrizione": $(`#desc`).val(),
-        "annotazione": $(`#ann`).val(),
-    };
+        // Create an FormData object
+        let data = new FormData(form);
 
-    for (input of $("input")) {
-        if (!(input.checkValidity())) {
-            $(input).css({ "border": "1px solid red" });
-            return;
-        }
-    }
-
-    await $.ajax({
-        url: serverUrl + "mongo/posthere?type=uffici",
-        type: 'POST',
-        data: JSON.stringify(formdata),
-        crossDomain: true,
-        contentType: 'application/json',
-        success: function (data) {
-            renderAddelem();
-            $("#textdiv").prepend(`<div class="success_upd">${data.msg}</div>`);
-            setTimeout(function () { $(".success_upd").remove() }, 10000);
-        },
-        error: function (data) {
-            $("#textdiv").prepend(`<div class="fail_upd">${data}</div>`);
-            setTimeout(function () { $(".fail_upd").remove() }, 10000);
-        }
-    });
-
+        $.ajax({
+            type: "POST",
+            enctype: 'multipart/form-data',
+            url: `${url}?type=uffici`,
+            data: data,
+            processData: false,
+            contentType: false,
+            success: function (data) {
+                renderAddelem();
+                $("#textdiv").prepend(`<div class="success_upd">${data.msg}</div>`);
+                setTimeout(function () { $(".success_upd").remove() }, 10000);
+            },
+            error: function (data) {
+                $("#textdiv").prepend(`<div class="fail_upd">Inserimento fallito</div>`);
+                setTimeout(function () { $(".fail_upd").remove() }, 10000);
+            }
+        });
+      });
 }
