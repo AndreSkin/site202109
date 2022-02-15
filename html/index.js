1b5c98d47a8f2d85c2e2c6c88b02dbf070e3db6d
Andrea Schinoppi <andrea.schinoppi@studio.unibo.it>
2022-02-15 11:50:45 +0100

    Revert "Accept only images in form"
    This reverts commit 34bd4e9a1af91b40199c27ac4b85d73f883a82a2.
    Commit per errore

--- html/index.js
+++ html/index.js
@@ -56,6 +56,11 @@ app.get('/', (req, res) => {
     )
 });
 
+app.get('/up', (req, res) => {
+    res.sendFile(
+        global.rootDir + '/up.html'
+    )
+});
 
 app.get('/management', (req, res) => {
     res.sendFile(
