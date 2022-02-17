9284e2bd7d4d80fe9ad7490e973d0f160bf72e17
Andrea Schinoppi <andrea.schinoppi@studio.unibo.it>
2022-02-17 15:10:05 +0100

    fix

--- Sorgenti/NoloNoloFront/.nuxt/vuetify/options.js
+++
@@ -1,1 +0,0 @@
-export default {"theme":{"dark":false,"themes":{"dark":{"primary":"#FFCA28","secondary":"#1976D2","accent":"#82B1FF","error":"#FF5252","info":"#2196F3","success":"#4CAF50","warning":"#FB8C00"}}}}
