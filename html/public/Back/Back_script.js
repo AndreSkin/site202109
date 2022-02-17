1d116cc65164db8e5d8b9f6fa5714b30133803fc
Andrea Schinoppi <andrea.schinoppi@studio.unibo.it>
2022-02-17 09:25:29 +0100

    Minor fixes

--- html/public/Back/Back_script.js
+++ html/public/Back/Back_script.js
@@ -51,9 +51,22 @@ async function setactive_side(id)
     }
 }
 
-async function renderHome()
+function renderHome()
 {
     $("#textdiv").empty();
     $("#textdiv").append(`<div><h1>Benvenuto nel Back Office di NoloNolo+ ${JSON.parse(localStorage.getItem('user')).nome}</div>`);
+    $("#textdiv").append(`
+        <div>
+        <h2>Cosa è possibile fare?</h2>
+        <ul>
+        <li><big>Per visualizzare l'inventario, le relative ìe disponibilità ed effettuare modifiche premere su Catalogo</big></li>
+        <li><big>Per visualizzare l'anagrafica dei clienti ed effettuare modifiche premere su Anagrafica</big></li>
+        <li><big>Per aggiungere un nuovo utente o un ufficio premere su Aggiungi</big></li>
+        <li><big>Per visualizzare i noleggi in corso o quelli terminati con relative fatture premere su Noleggi</big></li>
+        <li><big>Per accedere al front office premere su Front office</big></li>
+        <li><big>Per accedere all'area manager premere su Area Manager</big> (potrebbe essere richiesto di autenticarsi)</li>
+        </ul>
+        </div>
+      `);
     $("#textdiv").attr("aria-label", "Contenuto principale: messaggio di saluto");
 }
