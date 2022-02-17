cafe189c2b8236c1f55f744a4f7b68ac1c4cdb7e
Andrea Schinoppi <andrea.schinoppi@studio.unibo.it>
2022-02-17 15:09:37 +0100

    Revert "fix"
    This reverts commit 3fc18753b8ef9729a399d22a0ddfd7ddfb1dc96e.

---
+++ Sorgenti/NoloNoloFront/.eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		node: true
+	},
+	parserOptions: {
+		parser: '@babel/eslint-parser',
+		requireConfigFile: false
+	},
+	extends: [
+		'@nuxtjs',
+		'plugin:nuxt/recommended'
+	],
+	plugins: [
+	],
+	// add your custom rules here
+	rules: {
+		'array-bracket-spacing': 'off',
+		'arrow-parens': 'off',
+		'camelcase': 'off',
+		'comma-dangle': 'off',
+		'computed-property-spacing': 'off',
+		'indent': ['error', 'tab'],
+		'yoda': 'off',
+		'no-tabs': 'off',
+		'object-curly-newline': 'off',
+		'object-curly-spacing': 'off',
+		'one-var': 'off',
+		'padded-blocks': 'off',
+		'quote-props': 'off',
+		'quotes': 'off',
+		'space-before-function-paren': 'off',
+		'space-in-parens': 'off',
+		'spaced-comment': 'off',
+		'template-curly-spacing': 'off',
+		'vue/html-indent': ['error', 'tab'],
+		'vue/singleline-html-element-content-newline': 'off',
+		"no-unused-vars": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
+	}
+}
---
+++ Sorgenti/NoloNoloFront/components/Item/Item.vue
@@ -0,0 +1,16 @@
+<template>
+	<v-container>
+		<v-row no-gutters>
+			<v-col cols="12" sm="6">
+				<v-card outlined>
+					<v-img :src="'https://site202109.tw.cs.unibo.it' + data.img.slice(2)" height="300" :alt="'immagine di:' + data.nome" />
+				</v-card>
+			</v-col>
+			<v-col cols="12" sm="6">
+				<FirstData :data="data" :permessi="permessi" />
+			</v-col>
+		</v-row>
+	</v-container>
+</template>
+
+<script src="./Item.js"/>
